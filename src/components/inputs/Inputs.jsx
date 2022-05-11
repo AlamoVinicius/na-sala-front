1dbf7555f09f77618a9823da999e8112e7030af3
Álamo Vinícius <alamo.cma22@gmail.com>
2022-05-10 23:25:21 -0300

    separar as funções do newReservation em um arquivo separado

--- src/components/inputs/Inputs.jsx
+++ src/components/inputs/Inputs.jsx
@@ -3,25 +3,25 @@ import React from "react";
 import styles from "./Inputs.module.css";
 
 export const InputEffect = ({
-  type,
-  textLabel,
-  name,
-  handleOnChange,
-  value
+	type,
+	textLabel,
+	name,
+	handleOnChange,
+	value
 }) => {
-  return (
-    <div className={styles.group}>
-      <input
-        required
-        className={styles.input_effect}
-        type={type}
-        name={name}
-        onChange={handleOnChange}
-        value={value}
-      />
-      <span className={styles.highlight}></span>
-      <span className={styles.bar}></span>
-      <label htmlFor={name}>{textLabel}</label>
-    </div>
-  );
+	return (
+		<div className={styles.group}>
+			<input
+				required
+				className={styles.input_effect}
+				type={type}
+				name={name}
+				onChange={handleOnChange}
+				value={value}
+			/>
+			<span className={styles.highlight}></span>
+			<span className={styles.bar}></span>
+			<label htmlFor={name}>{textLabel}</label>
+		</div>
+	);
 };
--- src/components/pages/newReservation/NewReservation.jsx
+++ src/components/pages/newReservation/NewReservation.jsx
@@ -1,45 +1,46 @@
 import { getStations } from "../../../services/api";
 
 export const submit = async (
-  e,
-  setNewReservation,
-  setStations,
-  newReservation,
-  user
+	e,
+	setNewReservation,
+	setStations,
+	newReservation,
+	user
 ) => {
-  e.preventDefault();
-  const stations = await getStations();
-  setStations(stations.data);
-  setNewReservation({
-    ...newReservation,
-    id_user: user.id,
-    usuario: user.username
-  });
+	e.preventDefault();
+	//const stations = await getStations();
+    const stations = 
+	setStations(stations.data);
+	setNewReservation({
+		...newReservation,
+		id_user: user.id,
+		usuario: user.username
+	});
 };
 
 export const startTime = (
-  e,
-  setNewReservation,
-  newReservation,
-  setFinalShowPickTime
+	e,
+	setNewReservation,
+	newReservation,
+	setFinalShowPickTime
 ) => {
-  const startDate = new Date(e.target.value);
-  setNewReservation({ ...newReservation, [e.target.name]: startDate });
-  startDate !== null ? setFinalShowPickTime(true) : setFinalShowPickTime(false);
+	const startDate = new Date(e.target.value);
+	setNewReservation({ ...newReservation, [e.target.name]: startDate });
+	startDate !== null ? setFinalShowPickTime(true) : setFinalShowPickTime(false);
 };
 
 export const finalTime = (newReservation, e, setNewReservation) => {
-    let finalDate = new Date(newReservation.StartDate).toUTCString();
-    finalDate = new Date(finalDate);
-    const day = finalDate
-      .getDate()
-      .toString()
-      .padStart(2, "0");
-    const month = (finalDate.getMonth() + 1).toString().padStart(2, "0");
-    const year = finalDate
-      .getFullYear()
-      .toString()
-      .padStart(2, "0");
-    finalDate = new Date(`${year}-${month}-${day}T${e.target.value}`);
-    setNewReservation({ ...newReservation, [e.target.name]: finalDate });
-}
+	let finalDate = new Date(newReservation.StartDate).toUTCString();
+	finalDate = new Date(finalDate);
+	const day = finalDate
+		.getDate()
+		.toString()
+		.padStart(2, "0");
+	const month = (finalDate.getMonth() + 1).toString().padStart(2, "0");
+	const year = finalDate
+		.getFullYear()
+		.toString()
+		.padStart(2, "0");
+	finalDate = new Date(`${year}-${month}-${day}T${e.target.value}`);
+	setNewReservation({ ...newReservation, [e.target.name]: finalDate });
+};
