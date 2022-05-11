1fcad6fcdb330049242562201b9b87e8bb5b7075
Álamo Vinícius <alamo.cma22@gmail.com>
2022-05-11 00:27:56 -0300

    refatoramento da função finalTime

--- src/components/pages/newReservation/NewReservation.jsx
+++ src/components/pages/newReservation/NewReservation.jsx
@@ -1,46 +1,38 @@
 import { getStations } from "../../../services/api";
 
 export const submit = async (
-	e,
-	setNewReservation,
-	setStations,
-	newReservation,
-	user
+  e,
+  setNewReservation,
+  setStations,
+  newReservation,
+  user
 ) => {
-	e.preventDefault();
-	//const stations = await getStations();
-    const stations = 
-	setStations(stations.data);
-	setNewReservation({
-		...newReservation,
-		id_user: user.id,
-		usuario: user.username
-	});
+  e.preventDefault();
+  const stations = await getStations();
+  setStations(stations.data);
+  setNewReservation({
+    ...newReservation,
+    id_user: user.id,
+    usuario: user.username
+  });
 };
 
 export const startTime = (
-	e,
-	setNewReservation,
-	newReservation,
-	setFinalShowPickTime
+  e,
+  setNewReservation,
+  newReservation,
+  setFinalShowPickTime
 ) => {
-	const startDate = new Date(e.target.value);
-	setNewReservation({ ...newReservation, [e.target.name]: startDate });
-	startDate !== null ? setFinalShowPickTime(true) : setFinalShowPickTime(false);
+  const startDate = new Date(e.target.value);
+  setNewReservation({ ...newReservation, [e.target.name]: startDate });
+  startDate !== null ? setFinalShowPickTime(true) : setFinalShowPickTime(false);
 };
 
 export const finalTime = (newReservation, e, setNewReservation) => {
-	let finalDate = new Date(newReservation.StartDate).toUTCString();
-	finalDate = new Date(finalDate);
-	const day = finalDate
-		.getDate()
-		.toString()
-		.padStart(2, "0");
-	const month = (finalDate.getMonth() + 1).toString().padStart(2, "0");
-	const year = finalDate
-		.getFullYear()
-		.toString()
-		.padStart(2, "0");
-	finalDate = new Date(`${year}-${month}-${day}T${e.target.value}`);
-	setNewReservation({ ...newReservation, [e.target.name]: finalDate });
+  const initialDate = new Date(newReservation.StartDate); 
+  const finalDate = new Date(      //  format yyyy/mm/dd hh:mm is ok
+    `${initialDate.getFullYear()}/${initialDate.getMonth() +
+      1}/${initialDate.getDate()} ${e.target.value}`
+  );
+  setNewReservation({ ...newReservation, [e.target.name]: finalDate });
 };
