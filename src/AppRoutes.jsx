ca77271da88d000c10f976b65bc48ba8fd37a0b4
AlamoVinicius <alamo.cma@gmail.com>
2022-06-11 17:47:32 -0300

    fix: build process

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 
 import HomePage from "./components/pages/HomePage";
 import Login from "./components/pages/Login";
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration"
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
@@ -13,4 +15,8 @@ root.render(
   </React.StrictMode>
 );
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
