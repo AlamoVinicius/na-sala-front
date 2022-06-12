ed955a6799bfc23b105b7421c0338c696261f253
AlamoVinicius <alamo.cma@gmail.com>
2022-06-12 10:29:20 -0300

    fix: laodin and error msg in my reservation page

--- src/components/pages/my-reservations/index.jsx
+++ src/components/pages/my-reservations/index.jsx
@@ -4,12 +4,15 @@ import { useLocation } from "react-router-dom";
 import { getMyBookings } from "../../../services/api";
 
 import NavBar from "../../navbar/Navbar";
-import { Container, Alert } from "react-bootstrap";
+import { Container, Alert, Spinner } from "react-bootstrap";
 import { MyReservationList } from "./MyReservation";
+import styles from "./MyReservation.module.css"
 
 const Myreservations = () => {
   const [showMsg, setShowMsg] = useState(false);
   const [bookings, setBookings] = useState([]);
+  const [isLoading, setIsLoading] = useState(true)
+  const [errorMsg, setErrorMsg] = useState('')
 
   let msg = null;
   let location = useLocation();
@@ -20,13 +23,21 @@ const Myreservations = () => {
     if (msg) {
       setShowMsg(true);
     }
-
-    const get = async () => {
+    
+    try {
+      const get = async () => {
       const user = JSON.parse(localStorage.getItem("user"));
       const bookings = await getMyBookings(user.username);
       setBookings(bookings.data);
+      setIsLoading(false)
     };
     get();
+  
+    } catch (error) {
+      console.log(error)
+      setErrorMsg('ocorreu algum erro ao buscar as informações')
+    }
+    
     setTimeout(() => {
       setShowMsg(false);
     }, 3000);
@@ -39,6 +50,12 @@ const Myreservations = () => {
         {showMsg && <Alert variant="success">{msg}</Alert>}
         <h2>Minhas reservas</h2>
         <p>Reservas a partir do dia atual</p>
+        {errorMsg && <Alert variant="danger">{errorMsg}</Alert>}
+        {isLoading && (
+            <div className={styles.spinner}>
+              <Spinner animation="border" />
+            </div>
+          )}
         {bookings.map(booking => {
           return <MyReservationList key={booking._id} bookings={booking} />;
         })}
